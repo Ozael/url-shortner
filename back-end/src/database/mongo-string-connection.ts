a2108334f4868123abf391d91150132e21592599
Ozael <ozaelvf@gmail.com>
2022-01-29 11:41:51 -0300

    Delete mongo-string-connection.ts

--- back-end/src/database/mongo-string-connection.ts
+++
@@ -1,3 +0,0 @@
-export const MONGO_URL = `mongodb+srv://shortener-api:YHelWJQuUSutq16v@url-shortener-dio.nnkii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-    // mongodb+srv://shortener-api:<password>@url-shortener-dio.nnkii.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
