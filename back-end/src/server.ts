61407ecf857211ca103b8a85b9a80380118cf11b
Ozael <ozaelvf@gmail.com>
2022-01-28 23:31:47 -0300

    first commit

---
+++ back-end/src/server.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    ;
+});
+
+
+
+
+
+
+
+
+
+
+
+app.listen(5000, () => console.log('app running...'));
---
+++ front-end/src/App.tsx
@@ -0,0 +1,18 @@
+
+
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>URL Shrinker</h1>
+      <form action="" method="post">
+        <label htmlFor="fullURL">URL</label>
+        <input type="url" name="fullUrl" id="fullUrl" />
+        <button type="submit">Shrink</button>
+      </form>
+    </header>
+  </div>
+);
+
+
+export default App;
